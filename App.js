12e5b1cf219df62fdf0a982eaaeef9c1c56f3dad
unknown <rautanokka@gmail.com>
2022-02-05 15:58:35 +0200

    osoiteappi tehty

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import MapView, { Marker } from "react-native-maps";
+
+const API_KEY = "GX9GhQOZHQTGeAwHLtl08c8EHklTTANW"
 
 export default function App() {
+  const [address, setAddress] = useState()
+  const [region, setRegion] = useState({ latitude: 60.200692, longitude: 24.934302, latitudeDelta: 0.0322, longitudeDelta: 0.0221 })
+
+
+  const switchLocation = async () => {
+    const response = await fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=${API_KEY}&location=${address}`)
+    const data = await response.json()
+    const latLng = data.results[0].locations[0].latLng
+    setRegion({ ...region, latitude: latLng.lat, longitude: latLng.lng })
+    setAddress("")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <MapView
+        style={{ flex: 1 }}
+        region={region}
+      >
+        <Marker coordinate={{ latitude: 60.201373, longitude: 24.934041 }} title='Haaga-Helia' />
+      </MapView>
+      <View style={styles.container}>
+        <TextInput style={styles.input} onChangeText={text => setAddress(text)} value={address} placeholder="Address here" />
+        <Button title="SHOW" onPress={() => switchLocation()} />
+      </View>
+    </>
+
   );
 }
 
@@ -16,5 +41,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: "100%"
   },
+  input: {
+    width: "50%",
+    borderWidth: 1,
+    borderColor: "grey",
+    marginBottom: 10,
+    padding: 1
+  }
 });
