8686ccfa3f6befbbcdfbf13570f6f8e1a2df3e9f
unknown <rautanokka@gmail.com>
2022-02-06 17:27:21 +0200

    Lisätty käyttäjän sijaintiominaisuus

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location"
 
 const LOCATION_API_KEY = "GX9GhQOZHQTGeAwHLtl08c8EHklTTANW"
 const RESTAURANTS_API_KEY = "DyDjeja69FYVYUTQuaf4IRXi3XimzTKd"
 
 export default function App() {
   const [address, setAddress] = useState()
-  const [region, setRegion] = useState({ latitude: 59.9795585, longitude: 25.4483983, latitudeDelta: 0.0322, longitudeDelta: 0.0221 })
+  const [region, setRegion] = useState()
   const [restaurants, setRestaurants] = useState()
 
   const getNearbyRestaurants = async (lat, lon) => {
@@ -16,6 +16,21 @@ export default function App() {
     const data = await response.json()
     setRestaurants(data.results)
   }
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== "granted") {
+        Alert.alert("No permission to get current location")
+        setRegion({ latitude: 59.9795585, longitude: 25.4483983, latitudeDelta: 0.0322, longitudeDelta: 0.0221 })
+        return
+      }
+      let currentLocation = await Location.getCurrentPositionAsync({})
+      const coords = { latitude: currentLocation.coords.latitude, longitude: currentLocation.coords.longitude }
+      setRegion({ ...coords, latitudeDelta: 0.0322, longitudeDelta: 0.0221 })
+      getNearbyRestaurants(coords.latitude, coords.longitude)
+    })();
+  }, [])
 
   const switchLocation = async () => {
     const response = await fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=${LOCATION_API_KEY}&location=${address},FI`)
@@ -28,14 +43,14 @@ export default function App() {
 
   return (
     <>
-      <MapView
+      {region && <MapView
         style={{ flex: 1 }}
         region={region}
       >
         {restaurants !== undefined && restaurants.map((restaurant, index) => {
-          return <Marker key={index} coordinate={{ latitude: restaurant.position.lat, longitude: restaurant.position.lon }} title={restaurant.poi.name} description={restaurant.address.freeformAddress}/>
+          return <Marker key={index} coordinate={{ latitude: restaurant.position.lat, longitude: restaurant.position.lon }} title={restaurant.poi.name} description={restaurant.address.freeformAddress} />
         })}
-      </MapView>
+      </MapView>}
       <View style={styles.container}>
         <TextInput style={styles.input} onChangeText={text => setAddress(text)} value={address} placeholder="Address here" />
         <Button title="SHOW" onPress={() => switchLocation()} />
